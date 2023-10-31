881ca9a149ebb8a17d5a05c04604aa22e94ba3b1
BlackFreshMint <freshmint2511@gmail.com>
2023-10-31 09:36:32 -0600

    upd calendar

--- agenda_cit.php
+++ agenda_cit.php
@@ -11,38 +11,38 @@
 <body>
 
     <div class="container">
-       <?php include 'vistas/nav3.php'?>
-        <div class="agenda"> 
+       <?php include 'views/nav3.php'?>
+        <div class="agenda">
             <h1 class="title">Agenda Tu Cita</h1>
         </div>
         <div class="pregunta2" id="CUENTA">
             <p>CUENTA</p>
-            <div class=" pregunta"> 
+            <div class=" pregunta">
                 <p class="description">¿Usted ya esta registrado/a en nuestro sistema de servicio de hospital?</p>
-                
+
                 <div class="buttons">
                     <a href="#" class="button si" id="siButton">Si</a>
-                    <a href="preag2.html" class="button no">No</a>
+                    <a href="views/preag2.php" class="button no">No</a>
                 </div>
             </div>
         </div>
 
         <div id="contenidoAdicional" style="display: none;">
-        
+
             <!-- Contenido adicional que se mostrará cuando el usuario haga clic en "Si" -->
         <div class="pregunta2" id="CUENTA">
             <p>CURP</p>
-            <div class=" pregunta"> 
+            <div class=" pregunta">
                     <div class="buttons">
                         <input type="text" class="styled-input">
                                 <a href="#" class="button_enviar">ENVIAR</a>
-                            
+
                     </div>
                 </div>
             </div>
         </div>
 
-        <?php include 'vistas/footer.php'?>
+        <?php include 'views/footer.php'?>
     </div>
 
     <script src="js/agenda_cit.js"></script>
