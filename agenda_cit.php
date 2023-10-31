6598604baae71ce611b9f603f802e3a267a406d2
Kolc77 <139392754+Kolc77@users.noreply.github.com>
2023-10-31 08:12:21 -0600

    Add files via upload

---
+++ agenda_cit.php
@@ -0,0 +1,50 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda Tu Cita</title>
+    <link rel="stylesheet" href="css/agenda_cit.css">
+</head>
+
+<body>
+
+    <div class="container">
+       <?php include 'vistas/nav3.php'?>
+        <div class="agenda"> 
+            <h1 class="title">Agenda Tu Cita</h1>
+        </div>
+        <div class="pregunta2" id="CUENTA">
+            <p>CUENTA</p>
+            <div class=" pregunta"> 
+                <p class="description">¿Usted ya esta registrado/a en nuestro sistema de servicio de hospital?</p>
+                
+                <div class="buttons">
+                    <a href="#" class="button si" id="siButton">Si</a>
+                    <a href="preag2.html" class="button no">No</a>
+                </div>
+            </div>
+        </div>
+
+        <div id="contenidoAdicional" style="display: none;">
+        
+            <!-- Contenido adicional que se mostrará cuando el usuario haga clic en "Si" -->
+        <div class="pregunta2" id="CUENTA">
+            <p>CURP</p>
+            <div class=" pregunta"> 
+                    <div class="buttons">
+                        <input type="text" class="styled-input">
+                                <a href="#" class="button_enviar">ENVIAR</a>
+                            
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <?php include 'vistas/footer.php'?>
+    </div>
+
+    <script src="js/agenda_cit.js"></script>
+</body>
+</html>
