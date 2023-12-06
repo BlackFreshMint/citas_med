a2a95d241596ef06261084f6cbbcc2a95b10e3fd
JonathanSajor <139500894+JonathanSajor@users.noreply.github.com>
2023-12-06 09:39:08 -0600

    mis_citas.php
    Actualizado!

--- mis_citas.php
+++ mis_citas.php
@@ -19,11 +19,13 @@
         <thead>
     
             <tr>
-                <td>No cita</td>
-                <td>Especialidad</td>
-                <td>Fecha</td>
-                <td>Hora</td>
-                <td>Configurar cita</td>
+                <td>Cita_id</td>
+                <td>Curp_paciente</td>
+                <td>Especialidad_id</td>
+                <td>Doctor_id</td>
+                <td>Dia</td>
+                <td>Observaciones</td>
+                <td></td>
             </tr>
         </thead>
         <tbody>
@@ -37,10 +39,12 @@
     <div id="modalAgregar" style="display:none;">
         <h3>Agregar cita</h3>
         <form id="formAgregarUsuario">
-            No cita: <input type="text" id="addCITA_ID"><br>
-            Especialidad: <input type="text" id="addESPECIALIDAD"><br>
-            Fecha: <input type="text" id="addFECHA"><br>
-            Hora: <input type="text" id="addHORA"><br><br>
+            Cita_id: <input type="text" id="addCita_id"><br>
+            Curp_paciente: <input type="text" id="addCurp_paciente"><br>
+            Especialidad_id: <input type="text" id="addEspecialidad_id"><br>
+            Doctor_id: <input type="text" id="addDoctor_id"><br>
+            Dia: <input type="text" id="addDia"><br>
+            Observaciones: <input type="text" id="addObservaciones"><br><br>
             <button type="button" onclick="guardarNuevoUsuario()">Guardar cita</button>
             <button type="button" onclick="cerrarModal('modalAgregar')" style="background-color: red;">Cancelar</button>
         </form>
@@ -49,10 +53,12 @@
     <div id="modalEditar" style="display:none;">
         <h3>Editar cita</h3>
         <form id="formEditarUsuario">
-            No cita: <input type="text" id="editCITA_ID"><br>
-            Especialidad: <input type="text" id="editESPECIALIDAD"><br>
-            Fecha: <input type="text" id="editFECHA"><br>
-            Hora: <input type="text" id="editHORA"><br><br>
+            Cita_id: <input type="text" id="editCita_id"><br>
+            Curp_paciente: <input type="text" id="editCurp_paciente"><br>
+            Especialidad_id: <input type="text" id="editEspecialidad_id"><br>
+            Doctor_id: <input type="text" id="editDoctor_id"><br>
+            Dia: <input type="text" id="editDia"><br>
+            Observaciones: <input type="text" id="editObservaciones"><br><br>
             <button type="button" onclick="guardarEdicion()">Guardar cita</button>
             <button type="button" onclick="cerrarModal('modalEditar')" style="background-color: red;">Cancelar</button>
         </form>
@@ -78,19 +84,23 @@
         }
 
         function guardarNuevoUsuario() {
-            var CITA_ID = document.getElementById('addCITA_ID').value;
-            var ESPECIALIDAD = document.getElementById('addESPECIALIDAD').value;
-            var FECHA = document.getElementById('addFECHA').value;
-            var HORA = document.getElementById('addHORA').value;
+            var Cita_id = document.getElementById('addCita_id').value;
+            var Curp_paciente = document.getElementById('addCurp_paciente').value;
+            var Especialidad_id = document.getElementById('addEspecialidad_id').value;
+            var Doctor_id = document.getElementById('addDoctor_id').value;
+            var Dia = document.getElementById('addDia').value;
+            var Observaciones = document.getElementById('addObservaciones').value;
 
             $.ajax({
                 type: 'POST',
-                url: 'agregar_usuario.php',
+                url: 'php/agregar_usuario.php',
                 data: {
-                    CITA_ID: CITA_ID,
-                    ESPECIALIDAD: ESPECIALIDAD,
-                    FECHA: FECHA,
-                    HORA: HORA
+                    Cita_id: Cita_id,
+                    Curp_paciente: Curp_paciente,
+                    Especialidad_id: Especialidad_id,
+                    Doctor_id: Doctor_id,
+                    Dia: Dia,
+                    Observaciones: Observaciones,
 
                 },
                 success: function(response){
@@ -101,29 +111,35 @@
             });
         }
 
-        function mostrarEditarUsuario(CITA_ID, ESPECIALIDAD, FECHA, HORA) {
-            document.getElementById('editCITA_ID').value = CITA_ID;
-            document.getElementById('editESPECIALIDAD').value = ESPECIALIDAD;
-            document.getElementById('editFECHA').value = FECHA;
-            document.getElementById('editHORA').value = HORA;
+        function mostrarEditarUsuario(Cita_id, Curp_paciente, Especialidad_id, Doctor_id, Dia, Observaciones) {
+            document.getElementById('editCita_id').value = Cita_id;
+            document.getElementById('editCurp_paciente').value = Curp_paciente;
+            document.getElementById('editEspecialidad_id').value = Especialidad_id;
+            document.getElementById('editDoctor_id').value = Doctor_id;
+            document.getElementById('editDia').value = Dia;
+            document.getElementById('editObservaciones').value = Observaciones;
             document.getElementById('modalEditar').style.display = 'block';
         }
 
         function guardarEdicion() {
-            var CITA_ID = document.getElementById('editCITA_ID').value;
-            var ESPECIALIDAD = document.getElementById('editESPECIALIDAD').value;
-            var FECHA = document.getElementById('editFECHA').value;
-            var HORA = document.getElementById('editHORA').value;
+            var Cita_id = document.getElementById('editCita_id').value;
+            var Curp_paciente = document.getElementById('editCurp_paciente').value;
+            var Especialidad_id = document.getElementById('editEspecialidad_id').value;
+            var Doctor_id = document.getElementById('editDoctor_id').value;
+            var Dia = document.getElementById('editDia').value;
+            var Observaciones = document.getElementById('editObservaciones').value;
 
 
             $.ajax({
                 type: 'POST',
                 url: 'php/guardar_edicion.php',
                 data: {
-                    CITA_ID: CITA_ID,
-                    ESPECIALIDAD: ESPECIALIDAD,
-                    FECHA: FECHA,
-                    HORA: HORA,
+                    Cita_id: Cita_id,
+                    Curp_paciente: Curp_paciente,
+                    Especialidad_id: Especialidad_id,
+                    Doctor_id: Doctor_id,
+                    Dia: Dia,
+                    Observaciones: Observaciones,
                     
                 },
                 success: function(response){
@@ -135,16 +151,16 @@
         }
 
         $(document).on('click', '.delete-btn', function() {
-            var CITA_ID = $(this).closest('tr').find('td:first').text();
-            eliminarUsuario(CITA_ID);
+            var Cita_id = $(this).closest('tr').find('td:first').text();
+            eliminarUsuario(Cita_id);
         });
 
-        function eliminarUsuario(CITA_ID) {
+        function eliminarUsuario(Cita_id) {
             $.ajax({
                 type: 'POST',
                 url: 'php/eliminar_usuario.php',
                 data: {
-                    CITA_ID: CITA_ID
+                    Cita_id: Cita_id
                 },
                 success: function(response){
                     alert('Usuario eliminado correctamente');
